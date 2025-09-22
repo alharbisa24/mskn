5b2b53fc387b1845c61581c8049a6b523542ea1b
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-09-22 11:59:44 +0300

    fix

--- mskn/lib/main.dart
+++ mskn/lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("demo"),
+        title: Text("demo s"),
       ),
       body: Column(
         children: [
